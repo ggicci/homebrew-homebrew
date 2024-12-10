b16df03493978ee65969fab12791334085255413
Ggicci <ggicci.t@gmail.com>
2024-12-10 05:04:09 +0000

    feat: ramen

---
+++ Formula/ramen.rb
@@ -0,0 +1,19 @@
+class Ramen < Formula
+    desc "🍜 An easier way to define and parse arguments in SHELL scripts. getopts alternative."
+    homepage "https://github.com/ggicci/ramen"
+    url "https://github.com/ggicci/ramen/releases/download/v0.1.0/ramen-v0.1.0-x86_64-apple-darwin.tar.gz"
+    
+    # shasum -a 256 FILE
+    sha256 "75ac4701edd212bc2e80338193a8aeb384fbf2f75df85ad1820f18295b8e1bee"
+
+    depends_on "kubernetes-cli"
+
+    def install
+        bin.install "ramen"
+    end
+
+    test do
+        # Test if the app is installed correctly
+        system "#{bin}/ramen", "--version"
+    end
+end
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # homebrew-homebrew
+
 Ggicci's personal homebrew.
+
+```bash
+brew tap ggicci/homebrew
+```
